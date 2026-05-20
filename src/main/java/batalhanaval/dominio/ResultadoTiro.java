80cd8b2cd13dc3669da00d89b8aa4a58f6601f2d
Rafael Souza K <rafahira55@gmail.com>
2026-05-20 17:26:14 +0000

    Criei a classe enum ResultadoTiro para representar o resultado de um tiro do jogo.

---
+++ src/main/java/batalhanaval/dominio/ResultadoTiro.java
@@ -0,0 +1,26 @@
+// Classe enum para tipar as respostas dos disparos (água, acerto, afundou)
+
+public enum ResultadoTiro{
+    // valores do enum
+    AGUA("Água", false),
+    ACERTO("Acerto", true),
+    AFUNDOU("Afundou", true);
+
+    // Atributos do enum
+    private final String descricao;
+    private final boolean atingiuNavio;
+
+    // Construtor do enum (privado por padrão)
+    ResultadoTiro(String descricao, boolean atingiuNavio) {
+        this.descricao = descricao;
+        this.atingiuNavio = atingiuNavio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isAcerto() {
+        return atingiuNavio;
+    }
+}
