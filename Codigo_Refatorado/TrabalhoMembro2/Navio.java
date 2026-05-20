f49478cb4cc42ff7351c99b8a1cd72a71c3ea01c
Rafael Souza K <rafahira55@gmail.com>
2026-05-20 05:11:26 +0000

    Construí a classe Navio com os comportamentos necessários.

---
+++ Codigo_Refatorado/TrabalhoMembro2/Navio.java
@@ -0,0 +1,38 @@
+// Em vez de matrizes de inteiros shipId[][] e checagens soltas, o navio guarda seu próprio estado de vida.
+
+public class Navio{
+    // Atributos da classe Navio
+    private int id;
+    private String nome;
+    private int tamanho;
+    private int hitsRecebidos;
+
+    // Construtor da classe Navio
+    public Navio(int id, String nome, int tamanho) {
+        this.id = id;
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.hitsRecebidos = 0; // Inicialmente, o navio não recebeu nenhum hit
+    }
+
+    public void registrarAcerto(){
+        this.hitsRecebidos++;
+    }
+
+    public boolean isAfundado(){
+        return this.hitsRecebidos >= this.tamanho;
+    }
+
+    // Getters para os atributos id, nome e tamanho
+    public int getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+}
