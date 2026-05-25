a0cee2c22e03e4f18e59063dc3b61059bcd1830d
Rafael Souza K <rafahira55@gmail.com>
2026-05-25 17:41:36 +0000

    adicionei o metodo posicionarFrota na classe abstrata Jogador.

--- src/main/java/batalhanaval/dominio/Jogador.java
+++ src/main/java/batalhanaval/dominio/Jogador.java
@@ -21,6 +21,35 @@ public abstract class Jogador{
         return tabuleiro;
     }
 
+    /**
+     * Posiciona a frota automaticamente de forma aleatória.
+     * @param tamanhosNavios Array com o tamanho de cada navio (ex: [5-8])
+     */
+    public void posicionarFrota(int[] tamanhosNavios) {
+        java.util.Random random = new java.util.Random();
+        int boardSize = this.tabuleiro.getGrid().length;
+
+        // Para cada tamanho de navio exigido pelas regras
+        for (int tamanho : tamanhosNavios) {
+            boolean posicionado = false;
+            
+            // Fica em loop tentando sortear até achar uma posição válida
+            while (!posicionado) {
+                // Sorteia o X e Y dentro do tamanho do tabuleiro
+                int x = random.nextInt(boardSize);
+                int y = random.nextInt(boardSize);
+                Coordenada c = new Coordenada(x, y);
+
+                // Sorteia a direção (50% de chance de ser Horizontal ou Vertical)
+                String direcao = random.nextBoolean() ? "H" : "V";
+
+                // Tenta adicionar no tabuleiro. 
+                // Se bater em outro navio, adicionarNavio retorna false e o while repete o sorteio!
+                posicionado = this.tabuleiro.adicionarNavio(c, tamanho, direcao);
+            }
+        }
+    }
+
     // metodo abstrato sem corpo, força as subclasses a implementarem sua própria lógica de ataque
     // A UI vai ler o teclado com Scanner e passar a Coordenada pronta para este método.
     // - O HumanPlayer apenas retornará essa coordenadaDaUI.
